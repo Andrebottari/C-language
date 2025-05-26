c4a5ff6867303d8d941d8a43b2e4791caa758a6c
André Bottari <andrebottari4m@gmail.com>
2025-05-26 14:48:45 -0400

    Salvando mudanças antes do pull

--- atividade1.c
+++ atividade1.c
@@ -1,7 +1,5 @@
-//1)Elabore um algoritmo que receba, por meio do teclado, dois valores, um para a variável “a” e um
-//para a variável “b”. Em seguida, faça os passos que julgar necessário para que ao final, a variável
-//“a” possua o valor que inicialmente estava em “b” e a variável “b” possua o valor que inicialmente
-//estava em “a”. Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+//1)Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
+//variável auxiliar.
 
 
 #include<stdio.h>
