dddd90c9b4bda3d3bfa29ddd5574ae22a0c9545d
André Bottari <andrebottari4m@gmail.com>
2025-05-26 14:38:51 -0400

    Troca de valores entre variáveis sem usar variável auxiliar

---
+++ atividade2.c
@@ -0,0 +1,19 @@
+//2)Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
+//variável auxiliar.
+
+
+#include<stdio.h>
+
+int main(){
+
+ int a=0,b=0;
+ 
+ printf("Digite o valor da variavel A: ");
+ scanf("%d", &a);
+
+ printf("Digite o valor da variavel B: ");
+ scanf("%d", &b);
+
+
+ printf("O valor de A: %d \n O valor de B: %d", b,a);
+}
