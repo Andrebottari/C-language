e6560e645d2ee4db6bcdf15cacc70dad4d33e713
André Bottari <andrebottari4m@gmail.com>
2025-05-26 14:34:38 -0400

    Troca de valores entre variáveis usando variável auxiliar

--- atividade1.c
+++ atividade1.c
@@ -1,3 +1,9 @@
+//1)Elabore um algoritmo que receba, por meio do teclado, dois valores, um para a variável “a” e um
+//para a variável “b”. Em seguida, faça os passos que julgar necessário para que ao final, a variável
+//“a” possua o valor que inicialmente estava em “b” e a variável “b” possua o valor que inicialmente
+//estava em “a”. Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+
+
 #include<stdio.h>
 
 int main(){
